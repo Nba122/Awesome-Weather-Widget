bd86795af6b0f3b79521470c0f296a948e1f2b05
Steven Caamano <diceroll123@gmail.com>
2012-12-27 05:04:37 -0500

    Cleans up addParameter function

--- js/weather.js
+++ js/weather.js
@@ -180,46 +180,42 @@ function update_last_accessed() {
 }
 update_last_accessed();
 
-function addParameter(url, parameterName, parameterValue){
+function addParameter(url, parameterName, parameterValue) {
+  replaceDuplicates = true;
 
-    replaceDuplicates = true;
+  if (url.indexOf('#') > 0) {
+    var cl = url.indexOf('#');
+    urlhash = url.substring(url.indexOf('#'), url.length);
+  } else {
+    urlhash = '';
+    cl = url.length;
+  }
 
-    if(url.indexOf('#') > 0){
-        var cl = url.indexOf('#');
-        urlhash = url.substring(url.indexOf('#'),url.length);
-    } else {
-        urlhash = '';
-        cl = url.length;
-    }
+  sourceUrl = url.substring(0,cl);
 
-    sourceUrl = url.substring(0,cl);
+  var urlParts = sourceUrl.split("?");
+  var newQueryString = "";
 
-
-
-    var urlParts = sourceUrl.split("?");
-    var newQueryString = "";
-
-    if (urlParts.length > 1)
-    {
-        var parameters = urlParts[1].split("&");
-        for (var i=0; (i < parameters.length); i++)
-        {
-            var parameterParts = parameters[i].split("=");
-            if (!(replaceDuplicates && parameterParts[0] == parameterName))
-            {
-                if (newQueryString == "")
-                    newQueryString = "?";
-                else
-                    newQueryString += "&";
-                newQueryString += parameterParts[0] + "=" + parameterParts[1];
-            }
+  if (urlParts.length > 1) {
+    var parameters = urlParts[1].split("&");
+    for (var i=0; (i < parameters.length); i++) {
+      var parameterParts = parameters[i].split("=");
+      if (!(replaceDuplicates && parameterParts[0] == parameterName)) {
+        if (newQueryString == "") {
+          newQueryString = "?";
+        } else {
+          newQueryString += "&";
         }
+        newQueryString += parameterParts[0] + "=" + parameterParts[1];
+      }
     }
-    if (newQueryString == "")
-        newQueryString = "?";
-    else
-        newQueryString += "&";
-    newQueryString += parameterName + "=" + parameterValue;
+  }
+  if (newQueryString == "") {
+    newQueryString = "?";
+  } else {
+    newQueryString += "&";
+  }
+  newQueryString += parameterName + "=" + parameterValue;
 
-    return urlParts[0] + newQueryString + urlhash;
+  return urlParts[0] + newQueryString + urlhash;
 }
